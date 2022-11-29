3fe44b199e87ae70ba0ab1b97a488b5f92f371cc
X-DSI <xmandsi@gmail.com>
2022-11-29 19:39:45 +0530

    🚧 WIP: NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const NavBar = () => {
   return (
     <div className="">
-      <div className="absolute inset-y-0 left-0 z-50 h-screen w-1/12 bg-orange-yellow">
-        <div className=" inset-y-0 left-0 mx-auto flex h-5/6 w-full flex-col flex-wrap content-center items-center justify-between pt-10 ">
-          <Link className="-rotate-90" href="/">
+      <div className="absolute inset-y-0 left-0 z-50 flex h-screen w-24 flex-col justify-evenly bg-orange-yellow">
+        <div className="inset-y-0 left-0 mx-auto flex w-full flex-col content-center items-center justify-between pt-10 text-lg font-light uppercase ">
+          <Link className="font-bold" href="/">
             DSI
           </Link>
           <Link className="-rotate-90" href="/">
@@ -19,7 +19,11 @@ const NavBar = () => {
             Contact
           </Link>
         </div>
-        <div></div>
+        <div className="flex flex-col content-center items-center justify-between">
+          <p>F</p>
+          <p>F</p>
+          <p>F</p>
+        </div>
       </div>
     </div>
   );
