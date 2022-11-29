99b2693e5e91fde0da01065d7e49518a09baf16e
X-DSI <xmandsi@gmail.com>
2022-11-29 12:41:15 +0530

    ✨ NEW: NavBar Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import NavBar from "../components/NavBar";
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 text-white ">
+          <NavBar />
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             X-DSI Portfolio
           </h1>
