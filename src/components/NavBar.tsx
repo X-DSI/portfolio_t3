c7901192e95ee64be500410a92b47cb90fbd730c
X-DSI <xmandsi@gmail.com>
2022-11-30 17:27:04 +0530

    🌈 STYLE: NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,34 @@
 import Link from "next/link";
 import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <div className="">
-      <div className="absolute inset-y-0 left-0 z-50 flex h-screen w-24 flex-col justify-evenly bg-orange-yellow">
-        <div className="inset-y-0 left-0 mx-auto flex w-full flex-col content-center items-center justify-between pt-10 text-lg font-light uppercase ">
-          <Link className="font-bold" href="/">
-            DSI
-          </Link>
-          <Link className="-rotate-90" href="/">
-            Work
-          </Link>
-          <Link className="-rotate-90" href="/">
-            About
-          </Link>
-          <Link className="-rotate-90" href="/">
-            Contact
-          </Link>
-        </div>
-        <div className="flex flex-col content-center items-center justify-between">
-          <p>F</p>
-          <p>F</p>
-          <p>F</p>
-        </div>
+    <div className="main-navbar-div inset-y-0 left-0 z-50 flex h-screen w-24 flex-col justify-between bg-orange-yellow">
+      <div className="navbar-links flex h-screen flex-col content-center items-center justify-evenly text-lg font-light uppercase">
+        <Link className="font-bold" href="/">
+          DSI
+        </Link>
+        <Link className="-rotate-90" href="/">
+          Work
+        </Link>
+        <Link className="-rotate-90" href="/">
+          About
+        </Link>
+        <Link className="-rotate-90" href="/">
+          Contact
+        </Link>
+      </div>
+      <div className="navbar-icons relative mb-16 flex flex-col content-center items-center justify-around">
+        <Link href="github.com">
+          <FaGithub className="m-3 h-8 w-8" />
+        </Link>
+        <Link href="github.com">
+          <FaInstagram className="m-3 h-8 w-8" />
+        </Link>
+        <Link href="github.com">
+          <FaLinkedin className=" m-3 h-8 w-8" />
+        </Link>
       </div>
     </div>
   );
