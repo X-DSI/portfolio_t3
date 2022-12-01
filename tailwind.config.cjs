d65e14a5bb672eb0f2ca3e3498ce6971ce5f9e66
X-DSI <xmandsi@gmail.com>
2022-12-01 10:29:17 +0530

    🌈 STYLE: tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#fed43a",
+      secondary: "#121212",
+      accent: "#252525",
+      white: "white",
+    },
     fontFamily: {
       "roc-grotesk": "roc-grotesk",
     },
