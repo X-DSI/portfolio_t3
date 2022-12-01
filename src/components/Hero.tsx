7079e445484f117f79d203c5827e553cfa10bfdc
X-DSI <xmandsi@gmail.com>
2022-12-01 16:49:01 +0530

    ⏫ UPDATE: Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className="h-[200vh]">Hero</div>;
+  return (
+    <div className="hero-section h-full">
+      <div className="hero-wrapper grid">
+        <div className="hero-text-field">
+          <div className="h1-wrapper text-9xl text-primary">
+            <h1>Hi there, I&apos;m</h1>
+          </div>
+          <div className="h1-wrapper text-9xl text-primary">
+            <h1>Edgar Deiner</h1>
+          </div>
+          <div className="h1-wrapper text-9xl text-primary">
+            <h1>UX / UI Designer</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
