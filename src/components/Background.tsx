fc8256732da26e1cfc2e90a9c8111de3f0b2d478
X-DSI <xmandsi@gmail.com>
2022-12-01 10:29:40 +0530

    ⏫ UPDATE: Background component

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Background = () => {
-  return <div className="grid grid-cols-2 bg-stone-400">Background</div>;
+  return (
+    <div className="background grid h-screen w-full grid-cols-2">
+      <div className="bg-secondary text-primary">Left Column</div>
+      <div className="bg-primary">Right Column</div>
+    </div>
+  );
 };
 
 export default Background;
