e81efcd3c8bf951a612c20e5042d4ebcebd22a68
X-DSI <xmandsi@gmail.com>
2022-12-06 10:41:04 +0530

    ⏫ UPDATE: Background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Background = () => {
   return (
     <div className="background fixed ml-24 grid h-screen w-full grid-cols-2">
-      <div className="bg-secondary text-primary">Left Column</div>
-      <div className="bg-primary">Right Column</div>
+      <div className="bg-secondary text-primary"></div>
+      <div className="bg-primary"></div>
     </div>
   );
 };
