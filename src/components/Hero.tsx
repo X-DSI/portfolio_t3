1f41083faf05171e647728e824e99f11fb4b2039
X-DSI <xmandsi@gmail.com>
2022-12-14 19:26:09 +0530

    🐞 FIX: reinstalled node modules

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import React from "react";
 import LaptopGuy from "../assets/img/latop-guy.png";
 
-// breaking due to pnpm not working
 const Hero = () => {
   return (
     <div className="hero-section h-full">
